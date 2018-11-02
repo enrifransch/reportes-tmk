64a90efbb1ab330c6fe19bbe530a705122785bef
enrifransch <enrifransch@gmail.com>
2018-11-02 13:59:33 -0600

    xlsx

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
-const xlsx = require('node-xlsx');
+const XLSX = require('xlsx');
 const fs = require('fs');
 const f1 = 'KOOMKIN_JR_GRAL_26_10_18.xlsx'
 const uuidv4 = require('uuid/v4');
- 
+
+const buf = fs.readFileSync(f1);
+const wb = XLSX.read(buf, {type:'buffer'});
+
+console.log(wb['E3605'])
 
 // Parse a file
-const workSheetsFromFile = xlsx.parse(`./${f1}`);
+// const workSheetsFromFile = xlsx.parse(`./${f1}`);
 // workSheetsFromFile[0].data[0] = workSheetsFromFile[0].data[0].push('id')
-const hoja1 = workSheetsFromFile[0];
-hoja1.data[0].push('id')
+// const hoja1 = workSheetsFromFile[0];
+// hoja1.data[0].push('id')
 
-for (let i = 1; i < hoja1.data.length; i++) {
-  hoja1.data[i].push(uuidv4())
-}
+// for (let i = 1; i < hoja1.data.length; i++) {
+//   hoja1.data[i].push(uuidv4())
+// }
 
-workSheetsFromFile[0] = hoja1;
+// workSheetsFromFile[0] = hoja1;
 
-console.log(workSheetsFromFile)
 
-// var buffer = xlsx.build([{name: "KOOMKIN_JR_GRAL_26_10_18(2)", data: workSheetsFromFile}]); // Returns a buffer
+// const buffer = xlsx.build([{name: "KOOMKIN_JR_GRAL_26_10_18(2)", data: workSheetsFromFile}]); 
 
 // fs.writeFile("KOOMKIN_JR_GRAL_26_10_18(2)", buffer, function(err) {
 //     if(err) {
