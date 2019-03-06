134f9a15ffa2dc2fe7a727be86bb51cfb9fb107d
gerardomdn95 <gerardo.mdn95@gmail.com>
2019-03-06 15:01:17 -0600

    limpieza de codigo

--- script.js
+++ script.js
@@ -1,34 +1,23 @@
 
-const f1 = 'KOOMKIN_JR_GRAL_26_10_18'
-const f2 = 'KOOMKIN_SEMIMASTER_26_10.xlsx'
+const f1 = 'Base0306'
 const uuidv4 = require('uuid/v4');
 const Excel = require('exceljs');
-const tp = require('tedious-promises');
-const dbConfig = require('./conf.json');
-const TYPES = require('tedious').TYPES;
-tp.setConnectionConfig(dbConfig); // global scope
 
-tp.sql('select top 10 * from catusuario')
-.execute()
-.then(console.log)
-.catch(console.error)
-
-// addId(f1);
-// addId(f2);
+addId(f1);
 
 function addId(filename) {
     const workbook = new Excel.Workbook();
     workbook.xlsx.readFile(filename + '.xlsx').then(wb => {
-        const sheet = wb.getWorksheet('Hoja1') 
+        const sheet = wb.getWorksheet('Hoja1')
         const count = sheet.rowCount;
         const columns = sheet.columns;
         const idCol = sheet.getColumn(13);
         idCol.values = uuids(count);
-        idCol.header = 'uuid' 
-        wb.xlsx.writeFile(filename + '_uuid.xlsx').then(res => {
-            console.log(res)
-        })
-    })
+        idCol.header = 'uuid'
+        wb.xlsx.writeFile(filename + '_uuid.xlsx').then(() => {
+            console.log('Success 🌝');
+        });
+    });
 }
 
 function uuids(n) {
@@ -38,4 +27,3 @@ function uuids(n) {
     }
     return arr;
 }
-    
